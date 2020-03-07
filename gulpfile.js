27ecdb77e6fd595cf6a361133008479b78cb9415
ellavrubel <webella@protonmail.com>
2020-03-07 17:06:59 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,42 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
-function copy(done) {
+function css_style(done) {
 
-  gulp.src('./SCSS/style.scss')
-     .pipe(rename({suffix: '.min'}) ); // rename - отдельно подключаемый плагин, через npm install --save-dev gulp-rename
-     // .pipe( gulp.dest('./CSS/') ); //    pipe() - добавление задачи к уже существующей, выполняется как Promise, последовательно
+  gulp.src('./scss/**/*.scss')       // отсюда берем файл
+      .pipe(sourcemaps.init())        // инициализация плагина
+     .pipe(sass({                     // конвертация из SASS в СSS
+       errorLogToConsole: true,       // вывод ошибок в консоль
+        outputStyle: 'compressed'     // минифицировать CSS code
+
+     }))
+      .on('error', console.error.bind(console))  // обработчик события
+      .pipe(autoprefixer({
+          cascade: false
+      }))
+     .pipe(rename({suffix: '.min'}) ) // rename - отдельно подключаемый плагин, через npm install --save-dev gulp-rename
+     .pipe(sourcemaps.write('./'))
+     .pipe( gulp.dest('./css/') );    // pipe() - добавление задачи к уже существующей, выполняется как Promise, последовательно
 
     done();
 }
-gulp.task(copy);
+
+        function print (done){
+        console.log('Horray!');
+            done()
+        }
+
+        function watchSass(){  // параметр не обязателен
+    gulp.watch('./scss/**/*', css_style); // 1. какие файлы отслеживать 2. какую при этом применять функцию
+        }
 
 
 
+    gulp.task('default', gulp.series(print, watchSass)); // прописывать задачи можно в том порядке в кот. нужно их вызывать
+
+
+// gulp.task(css_style);
 // exports.default = defaultSomeTask;
